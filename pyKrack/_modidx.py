e00f578929c7fee4cbe94929cbd6a26cb8a9dbd4
FerranC96 <ferricaro@hotmail.com>
2023-04-04 12:14:27 +0100

    Test requirements in .ini and graphviz

--- pyKrack/_modidx.py
+++ pyKrack/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://FerranC96.github.io',
                 'git_url': 'https://github.com/FerranC96/pyKrack',
                 'lib_path': 'pyKrack'},
-  'syms': {'pyKrack.core': {'pyKrack.core.foo': ('core.html#foo', 'pyKrack/core.py')}}}
+  'syms': { 'pyKrack.core': {'pyKrack.core.foo': ('core.html#foo', 'pyKrack/core.py')},
+            'pyKrack.krackhardt': { 'pyKrack.krackhardt.compute_hierarchy': ('krackhardt.html#compute_hierarchy', 'pyKrack/krackhardt.py'),
+                                    'pyKrack.krackhardt.graph_properties': ('krackhardt.html#graph_properties', 'pyKrack/krackhardt.py')}}}
