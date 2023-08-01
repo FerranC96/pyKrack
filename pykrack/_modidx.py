fccc9e67d82285c58fcbe2797be21115cc2b9a82
FerranC96 <ferricaro@hotmail.com>
2023-08-01 15:51:39 +0100

    changed module names. WIP dependency import fix

--- pykrack/_modidx.py
+++ pykrack/_modidx.py
@@ -6,4 +6,5 @@ d = { 'settings': { 'branch': 'main',
                 'git_url': 'https://github.com/FerranC96/pykrack',
                 'lib_path': 'pykrack'},
   'syms': { 'pykrack.core': {'pykrack.core.graph_properties': ('core.html#graph_properties', 'pykrack/core.py')},
-            'pykrack.hierarchy': {'pykrack.hierarchy.compute_hierarchy': ('hierarchy.html#compute_hierarchy', 'pykrack/hierarchy.py')}}}
+            'pykrack.hierarchy': {'pykrack.hierarchy.compute_hierarchy': ('hierarchy.html#compute_hierarchy', 'pykrack/hierarchy.py')},
+            'pykrack.utils': {'pykrack.utils.graph_properties': ('core.html#graph_properties', 'pykrack/utils.py')}}}
